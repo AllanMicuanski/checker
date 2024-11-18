35f5c857bff36c69f8367d18a1c1b348c82413c8
AllanMicuanski <allanmicuanski@gmail.com>
2024-11-18 16:16:07 -0300

    remove scrape file

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
-const verificar = require("./checker");
+const search = require("./checker");
 
 const app = express();
 
 const port = process.env.port || 3000;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
-});
-
 app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+  res.send("Hello, I'm working :D");
 });
 
-app.get("/verificar", verificar);
+app.get("/search", search);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
