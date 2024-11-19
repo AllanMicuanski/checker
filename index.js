dadbf982b3421da41825f301b715730ac0a86b58
AllanMicuanski <allanmicuanski@gmail.com>
2024-11-19 10:32:03 -0300

    feat:added helmet and rateLimit

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 const express = require("express");
 const search = require("./checker");
 const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
-const port = process.env.port || 3000;
+// Configurações
+const port = process.env.PORT || 3000;
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 
-app.use(cors());
+// Middlewares
+app.use(helmet());
+app.use(cors({ origin: "https://simple-request-frontend.vercel.app" }));
+app.use(limiter);
 
+// Função para validar URLs
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+// Rotas
 app.get("/", (req, res) => {
   res.send("Hello, I'm working :D");
 });
 
-app.get("/search", search);
+// Rota de busca com validação de URL
+app.get(
+  "/search",
+  (req, res, next) => {
+    const { url } = req.query;
+    if (!url || !isValidUrl(url)) {
+      return res.status(400).json({ error: "URL inválida!" });
+    }
+    next();
+  },
+  search
+);
 
+// Iniciar servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
